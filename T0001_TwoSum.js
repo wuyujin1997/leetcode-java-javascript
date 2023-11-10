7f01bb5c206165817f70d20ea7590614b1fb9043
yujin-mac <wuyujin1997@outlook.com>
2023-11-10 22:40:14 +0800

    1. 两数之和 时间击败35%，内存击败86%。

---
+++ T0001_TwoSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+    return [0, 0];
+};
+
+var nums = [1,3,5,7,10];
+var target = 11;
+console.log(twoSum(nums, target));
